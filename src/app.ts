99ebf9fcc0065d7c4b0209d725a8517ad18cd03b
nurulla-hasan <nurullahasan.dev@gmail.com>
2026-05-19 12:35:09 +0600

    Global Error Handling Architecture

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,8 @@ import { profileRoute } from "./module/profile/profile.route";
 import { authRoute } from "./module/auth/auth.route";
 import logger from "./middleware/logger";
 import cookie from 'cookie-parser'
+import cors from 'cors'
+import globalErrorhandler from "./middleware/globalErrorHandler";
 
 const app: Application = express();
 
@@ -16,6 +18,15 @@ app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger);
+
+
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+}))
+
+
+
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
@@ -26,6 +37,13 @@ app.get("/", (req: Request, res: Response) => {
 app.use("/api/users", userRoute);
 app.use("/api/profile", profileRoute);
 app.use("/api/auth", authRoute);
+
+
+
+
+
+app.use(globalErrorhandler);
+
 
 
 
